ecd1b0d244a463a18eb46882ec6fe0d10d79c2ba
Francisco Junior <franciscojn49@gmail.com>
2021-02-14 20:33:20 -0300

    feat: added intial values reducer.

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,6 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-function cart() {
-  return [];
-}
+import { Reducer } from 'redux';
+import { ICartState } from './props';
+
+const INITIAL_STATE: ICartState = {
+  items: [],
+};
+
+const cart: Reducer<ICartState> = () => {
+  return INITIAL_STATE;
+};
 
 export default cart;
